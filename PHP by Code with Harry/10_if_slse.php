3ed98889f9a8b125d356b40ecedce25a10230d19
Ubaid Sooomro <107256782+Ubaidsoomro12@users.noreply.github.com>
2024-07-08 13:20:39 +0500

    Add files via upload

---
+++ PHP by Code with Harry/10_if_slse.php
@@ -0,0 +1,29 @@
+<?php
+        // Example_1;
+// $a=50;
+// $b=40;
+// if($a > $b){
+//     echo "a is greater than b";
+
+// }else{
+// echo "b is greater than a";
+// }
+
+            // Example_2;
+$age = 40;
+if($age >= 15 && $age <= 18){
+    echo "You are eligible to vote";
+}elseif($age >= 18 && $age <= 20){
+    echo"You are now to Eligible for driving Licinces";
+}
+elseif($age >= 20 && $age <= 40 ){
+    echo "You mus have licinse while using driving";
+}
+else{
+    echo "you should have Rest";
+}
+
+
+
+
+?>
