bd080733d49a9c930e9dd1eceb4086129363bf5a
Ubaid Sooomro <107256782+Ubaidsoomro12@users.noreply.github.com>
2024-07-10 12:55:21 +0500

    Add files via upload

---
+++ PHP by Code with Harry/15-foreach_Loops.php
@@ -0,0 +1,17 @@
+<?php
+echo"First way to print array values uning for loops: <br><br>";
+$arr = ["Ubaid","junaid","Vishal","Sami"];
+for($i=0; $i < count($arr); $i++){
+    echo $arr[$i]."<br>";
+    
+}
+echo"better way to print an array by using (Foreach Loops): <br><br>";
+foreach($arr as $v){ // All items of the first variable($arr) store in second variable($v) and print sequence of all items in the $arr
+    echo $v."<br>";
+    
+}
+//The most common use of the foreach loop, is to loop through the items of an array.
+//For every loop iteration, the value of the current array element is assigned to the variabe $x. The iteration continues until it reaches the last array element.
+
+
+?>
