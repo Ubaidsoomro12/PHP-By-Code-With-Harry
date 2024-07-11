d4256a173884635bc3c536de43d2299497dfb9f2
Ubaid Sooomro <107256782+Ubaidsoomro12@users.noreply.github.com>
2024-07-11 13:23:24 +0500

    Add files via upload

---
+++ 17_Date_Function.php
@@ -0,0 +1,8 @@
+<?php
+// ------------------------------- PHP date() Function------------------
+// The date() function returns the date and time as a string, formatted according to the specified format
+
+ $d=date("D j-F-Y");
+echo "Today is:  $d";
+
+?>
