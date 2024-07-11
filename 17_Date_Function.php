2706fef00cb9a298ec7307529be3e9c92b313408
Ubaid Sooomro <107256782+Ubaidsoomro12@users.noreply.github.com>
2024-07-11 13:26:30 +0500

    Delete 17_Date_Function.php

--- 17_Date_Function.php
+++
@@ -1,8 +0,0 @@
-<?php
-// ------------------------------- PHP date() Function------------------
-// The date() function returns the date and time as a string, formatted according to the specified format
-
- $d=date("D j-F-Y");
-echo "Today is:  $d";
-
-?>
